1d3c1623b0120ec247232be276106e32417db885
Paul Mayer <paul@paul-mayer.eu>
2023-02-27 09:41:18 +0100

    add cron job for running test

--- cypress/e2e/kvr.cy.js
+++ cypress/e2e/kvr.cy.js
@@ -2,16 +2,19 @@ var dayjs = require("dayjs");
 const timeString = "MM-DD-HH:mm:ss";
 describe("KVR", () => {
   it("first month", () => {
-    cy.visit("https://terminvereinbarung.muenchen.de/bba/termin/?loc=BB");
+    cy.visit("https://terminvereinbarung.muenchen.de/fs/termin/?loc=FS&ct=1071898");
     cy.viewport(1200, 1000); // Set viewport to 550px x 750px
-    cy.get(
-      '[href="javascript:toggle(\'Meldeangelegenheiten\');"] > h3'
-    ).click();
-    /*cy.get(
-      "[href=\"javascript:toggle('Ortskundeprüfung');\"] > h3"
-    ).click();*/
-    cy.get('[name="CASETYPES[FS Umschreibung Ausländischer FS]"]').select("1");
+
+    // cy.get(
+    //   '#Umschreibung_SPACE_eines_SPACE_ausländischen_SPACE_Führerscheins'
+    // ).click();
+    // /*cy.get(
+    //   "[href=\"javascript:toggle('Ortskundeprüfung');\"] > h3"
+    // ).click();*/
+    // cy.get('#').select("1");
     //cy.get('[name="CASETYPES[FS Anmeldung und Vereinbarung Prüftermin]"]').select("1");
+    cy.get(".WEB_APPOINT_FORWARDBUTTON").should("exist");
+
     cy.get(".WEB_APPOINT_FORWARDBUTTON").click();
 
     cy.get(".nat_calendar_weekday_bookable").should("not.exist");
@@ -22,7 +25,7 @@ describe("KVR", () => {
       .click();
 
     //cy.get(".nat_calendar")
-    cy.get(".nat_calendar_weekday_bookable").should("not.exist");
+    //cy.get(".nat_calendar_weekday_bookable").should("not.exist");
   });
 });
 
